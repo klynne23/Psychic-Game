95832bcb637f0c05084a887b2886d7c069be8c7d
Kirthi Bisset <kirthi.bisset@yahoo.com>
2018-12-06 17:11:27 -0500

    minor fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -55,7 +55,7 @@ document.onkeydown = function (event) {
         compLetter = String.fromCharCode(Math.floor(Math.random() * 26) + 97)
         document.getElementById("cl").innerHTML = compLetter;
         // location.reload();
-        
+
     }
 
     // DISPLAY NEW VARIABLES IN BROSWER
